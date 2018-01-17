2139e7f47cfe3193c92919bb91da53d5428a7d0c
artsiom.kushniarou <kushnerovartem@gmail.com>
2018-01-17 19:49:35 +0100

    Add counter

--- main.py
+++ main.py
@@ -21,10 +21,13 @@ def main():
     with open(done_list_path, 'a+') as done_list_file:
         apps_to_process = apps.get_apps_to_process(config.APK_REPOSITORY, done_list_file)
         logging.info("Start experiment")
+        app_count = len(apps_to_process)
+        counter = 0
         for app_name in apps_to_process:
             try:
+                counter += 1
                 apk = Apk(app_name)
-                logging.info(f"Testing {app_name}")
+                logging.info(f"Testing {app_name} {counter } of {app_count}")
                 adbhelper.install(apk.path)
                 agent.run_main_activity(apk)
                 print("Press: c - crashed, s - successed")
