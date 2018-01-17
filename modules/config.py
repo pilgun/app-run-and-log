9b60bd2116c4339a2be60e95e3285c000cc7f3b6
artsiom.kushniarou <kushnerovartem@gmail.com>
2018-01-17 19:03:02 +0100

    Remove config file

--- modules/config.py
+++
@@ -1,5 +0,0 @@
-APK_REPOSITORY = r"C:\fdroid_results"
-result_json_file = 'results.json'
-
-#macos: adb_path =  "/Users/alexandrpilgun/Library/Android/sdk/platform-tools/adb"
-adb_path = r"C:\Users\aleksandr.pilgun\appdata\local\android\sdk\platform-tools\adb.exe"
